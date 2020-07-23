3f872d12d5e9fe2de060a89809c737173db9cc20
Minh Tran <trancongminh503@gmail.com>
2020-07-23 15:58:24 +0700

    [config] Changed StringLength AuditableEntity Max to 256

--- ThenDelivery/Server/Persistence/Migrations/20200723085658_InitDbV2.Designer.cs
+++ ThenDelivery/Server/Persistence/Migrations/20200723085658_InitDbV2.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ThenDelivery.Server.Data;
 
 namespace ThenDelivery.Server.Persistence.Migrations
 {
     [DbContext(typeof(ThenDeliveryDbContext))]
-    partial class ThenDeliveryDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200723085658_InitDbV2")]
+    partial class InitDbV2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
